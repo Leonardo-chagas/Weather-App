865b442a043c8bc9b5f604b6ae23e8bcdc506da7
leonardo-chagas <leoschagas@hotmail.com>
2025-06-20 20:02:29 -0300

    definições dos componentes, estrutura inicial

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,37 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
+import CurrentDay from './Components/CurrentDay';
+import ForecastDay from './Components/ForecastDay';
 //import dotenv from 'dotenv';
 
 function App() {
   //dotenv.config();
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+  const [city, setCity] = useState('');
+  const [inputValue, setInputValue] = useState('');
+
   const apiKey = import.meta.env.VITE_API_KEY;
   console.log(apiKey);
 
+  useEffect(() => {
+    axios.get('http://api.weatherapi.com/v1/forecast.json?key='+apiKey+'&q='+city+'&days=7&aqi=no&alerts=no').then(
+      response => {
+        setData(response.data);
+      }
+    ).catch(error=> {
+      console.error(error);
+    });
+    console.log(data);
+  }, [city]);
+
+  const handleCityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input placeholder='Search city' onChange={handleCityChange}></input>
+        <button onClick={() => setCity(inputValue)}>Search</button>
+      <CurrentDay/>
+      <div id={'forecastContent'}>
+        <ForecastDay/>
+        <ForecastDay/>
+        <ForecastDay/>
+        <ForecastDay/>
+        <ForecastDay/>
+        <ForecastDay/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
